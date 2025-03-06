32aa190d9c374a84c9cdf974cc33060b4a6fd226
jcarpio <jose.carpio@gmail.com>
2025-03-06 12:24:01 +0100

    Update wedding_woes_v2.pl

--- exercism/wedding_woes_v2.pl
+++ exercism/wedding_woes_v2.pl
@@ -5,9 +5,9 @@ likes(esteban, malena).
 likes(malena, esteban).
 likes(gustavo, valeria).
 
-pairing(A, _):- chatty(A).
-pairing(_, A):- chatty(A).
+pairing(A, _):- chatty(A), A \= B.
+pairing(_, A):- chatty(A), A \= B.
 % a /\ b -> c
-pairing(A, B):- likes(A,B), likes(B,A). 
+pairing(A, B):- likes(A,B), likes(B,A), A \= B. 
 
 seating(A, B, C, D, E):- pairing(A,B), pairing(B, C), pairing(C,D), pairing(D, E). 
