1632b3725e47b7bbe05dc62ec4d85c8cacae5b38
jcarpio <jose.carpio@gmail.com>
2025-03-21 15:38:38 +0100

    Create isogram.pl

---
+++ isogram.pl
@@ -0,0 +1,21 @@
+:- use_module(library(clpfd)).
+
+/*
+ isogram(String)
+   es cierto si la cadena String contiene caracteres diferentes
+   excepto los espacios en blanco y los guiones que si se podrán
+   repetir.
+
+
+*/
+
+
+/*
+quita_blancos(Lista, R)
+  es cierto si R unifica con la lista de números que
+  contiene Lista eliminando las ocurrencias de 32 y 35 (" " y "-"). 
+  
+*/
+
+blanco(32).
+blanco(45).
