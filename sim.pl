f226a3404941af2b3d70eb4f3334200fd736433e
José <jose.carpio@gmail.com>
2025-05-23 11:53:37 +0200

    Update sim.pl

--- sim.pl
+++ sim.pl
@@ -76,3 +76,20 @@ slots_couplings(Slots, F-S) :-
  
  list_without_nths(Lista, [Cab|Resto], R2):-
    list_without_nths(Lista, Resto, R), elimina_pos(R, Cab, R2).
+
+/*  
+ elimina_pos(+Lista, +Pos, -R)
+   es cierto si R unifica con una lista que contiene los elementos
+   de Lista exceptuando el que ocupa la posición Pos. Los
+   valores de posiciones empiezan en 0.
+*/
+
+ elimina_pos([], _, []).
+ 
+ elimina_pos([_|Resto], 0, Resto).
+ 
+ elimina_pos([Cab|Resto], Pos, [Cab|R]):- Pos > 0, Pos2 #= Pos - 1,
+   elimina_pos(Resto, Pos2, R).
+            
+%:- list_without_nths("abcd", [3], "abc").
+%:- list_without_nths("abcd", [1,2], "ad"). 
